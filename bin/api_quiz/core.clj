63046f83b5b879b9ee7159fa738fbd726b76ca39
Andrey Bakuta <anb@anahoret.com>
2012-12-04 22:38:53 +0200

    Google

--- bin/api_quiz/core.clj
+++ bin/api_quiz/core.clj
@@ -5,7 +5,18 @@
  (:use api-quiz.fibonacci)
  (:use api-quiz.sha1)
  (:require [compojure.route :as route]
-           [clojure.data.json :as json]))
+           [clojure.data.json :as json])
+ (:import (java.net URL)
+          (java.lang StringBuilder)
+          (java.io BufferedReader InputStreamReader)))
+
+(defn fetch-url
+  "Return the web page as a string."
+  [address]
+  (let [url (URL. address)]
+    (with-open [stream (. url (openStream))]
+      (let [buf (BufferedReader. (InputStreamReader. stream))]
+        (apply str (line-seq buf))))))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
@@ -15,6 +26,7 @@
 (defroutes api-routes
   (GET "/" [] {:status 200})
   (GET "/fib/:num" [num] (json-response (fib-iter (biginteger num))))
+  (GET "/google-body" [] (json-response (sha1 (fetch-url "http://google.com"))))
   (POST "/store/:val" [val] (str "post test val was " val))
   (route/not-found "Page not found"))
 

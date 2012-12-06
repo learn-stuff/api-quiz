1cb03f0007fcea900c174ddb936d97144af3643a
Andrey Bakuta <dra1n86@gmail.com>
2012-12-06 12:54:49 +0200

    Fix integer overflow error.

--- bin/api_quiz/fibonacci.clj
+++ bin/api_quiz/fibonacci.clj
@@ -2,6 +2,6 @@
 
 (defn fib-iter [n]
   (if (< n 3) 1
-    (loop [x (biginteger 1), y 1, z 2, counter (- n  3)]
-      (if (= 0 counter) z
-        (recur y z (+ y z) (dec counter))))))
+    (loop [x (biginteger 1), y 2, counter (- n  3)]
+      (if (= 0 counter) y
+        (recur y (+ x y) (dec counter))))))
